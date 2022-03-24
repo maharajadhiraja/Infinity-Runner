08eccc46173b95df8a49d8dcafad0afd51f2bc92
Aryan Patel <76842388+maharajadhiraja@users.noreply.github.com>
2022-03-24 23:22:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,7 +72,7 @@ function setup() {
   restart = createSprite(450,250)
   restart.addImage(resetimg)
   restart.scale=0.3
-  restart.visible=false
+  restart.visible=true
   enemygroup1 = new Group()
   enemygroup3 = new Group()
   enemygroup2 = new Group()
